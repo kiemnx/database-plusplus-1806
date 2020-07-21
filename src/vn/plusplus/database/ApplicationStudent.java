f85a6a91d08b4076dd907c92c9cc41287d6fd669
kiemnx <kiemnguyenxuan@gmail.com>
2020-07-21 21:38:52 +0700

    Chua bai 53

--- src/vn/plusplus/database/ApplicationStudent.java
+++ src/vn/plusplus/database/ApplicationStudent.java
@@ -1,7 +1,35 @@
 package vn.plusplus.database;
 
+import vn.plusplus.database.services.ManageStudentService;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ApplicationStudent {
     public static void main(String[] args) {
+        System.out.println("-------- MySQL JDBC Connection Demo ------------");
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+        }
+        catch (ClassNotFoundException e) {
+            System.out.println("MySQL JDBC Driver not found !!");
+            return;
+        }
+        System.out.println("MySQL JDBC Driver Registered!");
+        Connection connection = null;
+        try {
+            connection = DriverManager
+                    .getConnection("jdbc:mysql://localhost:3306/manage_student?characterEncoding=utf8", "root", "kiemnx");
+            System.out.println("SQL Connection to database established!");
+
+        } catch (SQLException e) {
+            System.out.println("Connection Failed! Check output console"  + e);
+            return;
+        }
+        ManageStudentService service = new ManageStudentService(connection);
+        service.updateAvegerScore();
 
     }
 }
